18ac926aaf19063dd1d1be9bf1eca400f76d7f24
prince-bodar <bodarprins@gmail.com>
2024-07-31 16:54:41 +0530

    31-07-2024

--- Backend Devloaping/Node Js/Server/index.js
+++ Backend Devloaping/Node Js/Server/index.js
@@ -1,13 +1,21 @@
 const http = require('http')
 const fs = require("fs")
+const url = require("url")
+
 const server = http.createServer((req,res)=>{
     // console.log(req);
+    if(req.url === "/favicon.ico") return res.end()
     const log = `${Date.now()}: ${req.url} : new req recive\n`
+    const myurl = url.parse(req.url,true)
+    console.log(myurl);
     fs.appendFile("log.text",log,(err,data) => {
-        switch(req.url){
+        switch(myurl.pathname){
               case "/" :  res.end("<h1>Hello this is home page</h1>") 
               break
-              case "/about" :  res.end("<h1>My name is  Bodar Prince</h1>")
+              case "/about" :
+                const username = myurl.query.myname
+                const usr_id = myurl.query.userid
+                res.end(`<h1>My name is  ${username} and my user id is ${usr_id}</h1>`)
               break
               default:res.end("<h1>404 Your page is not Found</h1>")
         }
