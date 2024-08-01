11a509b9f31b6803fbe571cc5a86bcbf996e58f2
prince-bodar <bodarprins@gmail.com>
2024-08-01 16:26:25 +0530

    01-08-2024

--- Backend Devloaping/Node Js/HTTP method/index.js
+++ Backend Devloaping/Node Js/HTTP method/index.js
@@ -1,42 +1,19 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express")
+const app = express()
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end();
 
-  const log = `${Date.now()}: ${req.method} : ${req.url} : new req recive\n`;
-  const myurl = url.parse(req.url, true);
+app.get('/',(req,res) => {
+  res.end(`<h1>This is Home Page</h1>`)
+})
 
-  fs.appendFile("log.text", log, (err, data) => {
+app.get('/signup',(req,res) => {
+  res.end(`<h1>This is Sign up Page your name is ${req.query.name} and id is ${req.query.id}</h1>`)
+})
 
-    switch (myurl.pathname) {
-      case "/":
-          if(req.method == "GET")  res.end("<h1>Hello this is home page</h1>");
-        break;
+app.get('/about',(req,res) => {
+  res.end("<h1>This is About Page</h1>")
+})
 
-      case "/signup" :
-        if(req.method == "GET")
-          res.end("this is sign up from")
-        else if(req.method == "POST")
-           res.end("Success")
-        break
-
-      case "/about":
-        const username = myurl.query.myname;
-        const usr_id = myurl.query.userid;
-        res.end(`<h1>Your name is  ${username} and Your user id is ${usr_id}</h1>`);
-        break;
-
-      case "/search":
-        const search = myurl.query.search_query;
-        res.end("<p>Here your result search for </p>" + search);
-        break
-      default:res.end("<h1>404 Your page is not Found</h1>");
-    }
-  });
-});
-
-server.listen(9090, () => {
-  console.log("your server is started on port http://localhost:9090");
+app.listen(9090, () => {
+  console.log("your server is starte d on port http://localhost:9090");
 });
